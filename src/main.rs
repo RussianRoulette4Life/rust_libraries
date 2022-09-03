006612e36c4a2d8ec3cc20e84d06c2fbc6052245
Ruslan <petrushin-ruslanrus@yandex.ru>
2022-09-03 23:07:35 +0300

    version 1.0 i64 sorting! i will try to make it crosscompatible with i32 or smth

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use std::io::stdin;
 fn main() {
-    let random_vector: Vec<i32> = vec![9,5,624,21,1,-1];
+    let mut thing: String = String::new();
+    stdin()
+        .read_line(&mut thing)
+        .expect("what");
+    let finalised_input = thing.trim();
+    let mut random_vector: Vec<i64> = vec![];
+    for number in finalised_input.split_whitespace() {
+        let mut addition: i64 = number.parse().expect("nah couldnt do it sorry, seperate numbers by white spaces only!");
+        random_vector.push(addition);
+    }
     let tuple_of_indexes = (0,1);
-    // let changed_vector: Vec<i32> = switch_index_i32(random_vector, tuple_of_indexes);
+    // let changed_vector: Vec<i64> = switch_index_i64(random_vector, tuple_of_indexes);
     // println!("{:#?}", changed_vector);
-    let sorted = sort_vector_i32(random_vector);
+    let sorted = sort_vector_i64(random_vector);
     println!("{:#?}, this one is sorted", sorted)
 }
-fn sort_vector_i32(mut vector_to_sort: Vec<i32>) -> Vec<i32> {
-    let mut vector_copy: Vec<i32> = vec![];
+pub fn sort_vector_i64(vector_to_sort: Vec<i64>) -> Vec<i64> {
+    let mut vector_copy: Vec<i64> = vec![];
     for num in &vector_to_sort {
         vector_copy.push(*num)
     }
@@ -17,7 +27,7 @@ fn sort_vector_i32(mut vector_to_sort: Vec<i32>) -> Vec<i32> {
     for num in &vector_to_sort{
         if index != 0 {
             if vector_to_sort[prev_index] > vector_to_sort[index] {
-                vector_copy = switch_index_i32(vector_copy, (prev_index,index)); 
+                vector_copy = switch_index_i64(vector_copy, (prev_index,index)); 
             }
         } else {
             println!("starting sort...")
@@ -27,12 +37,12 @@ fn sort_vector_i32(mut vector_to_sort: Vec<i32>) -> Vec<i32> {
 
     }
     if vector_to_sort != vector_copy {
-        sort_vector_i32(vector_copy)
+        sort_vector_i64(vector_copy)
     } else {
         return vector_copy;
     }
 }
-fn switch_index_i32(mut vector: Vec<i32>, indexes_to_switch: (usize,usize)) -> Vec<i32>{
+fn switch_index_i64(mut vector: Vec<i64>, indexes_to_switch: (usize,usize)) -> Vec<i64>{
     let first_index = indexes_to_switch.0;
     let second_index = indexes_to_switch.1;
     let first_num = vector[first_index];
